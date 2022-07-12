8c2cde78f01e1929f7049c7f42c513d9eee8c936
amandadumi <amanda.e.dumi@gmail.com>
2022-07-12 14:55:22 -0400

    fixing tdtest file against master

--- test/data/testTD.py
+++ test/data/testTD.py
@@ -13,6 +13,7 @@ import unittest
 import numpy
 
 from skip import skipForParser
+from skip import skipForLogfile
 
 
 __filedir__ = os.path.realpath(os.path.dirname(__file__))
@@ -26,7 +27,7 @@ class GenericTDTest(unittest.TestCase):
 
     @skipForParser('CFOUR','The parser is still being developed so we skip this test')
     @skipForParser('Molcas','The parser is still being developed so we skip this test')
-    @skipForParser('Turbomole','The parser is still being developed so we skip this test')
+    @skipForLogfile('Turbomole/basicTurbomole7.4/CO_cc2_TD_trip', 'Oscillator strengths are not available for Turbomole triplets using ricc2 but are required for testenergies()')
     def testenergies(self):
         """Is the l_max reasonable?"""
 
@@ -39,7 +40,7 @@ class GenericTDTest(unittest.TestCase):
 
     @skipForParser('CFOUR','The parser is still being developed so we skip this test')
     @skipForParser('Molcas','The parser is still being developed so we skip this test')
-    @skipForParser('Turbomole','The parser is still being developed so we skip this test')
+    @skipForLogfile("Turbomole/basicTurbomole7.4/CO_cc2_TD_trip", "Oscillator strengths are not available for triplets with Turbomole's ricc2")
     def testoscs(self):
         """Is the maximum of etoscs in the right range?"""
         self.assertEqual(len(self.data.etoscs), self.number)
@@ -56,8 +57,8 @@ class GenericTDTest(unittest.TestCase):
         self.assertAlmostEqual(sumofsec, 1.0, delta=0.16)
 
     @skipForParser('CFOUR','The parser is still being developed so we skip this test')
-    @skipForParser('FChk', 'This is true for calculations without symmetry, but not with?')
     @skipForParser('DALTON', 'This is true for calculations without symmetry, but not with?')
+    @skipForParser('FChk', 'This is true for calculations without symmetry, but not with?')
     @skipForParser('Molcas','The parser is still being developed so we skip this test')
     def testsecs_transition(self):
         """Is the lowest E transition from the HOMO or to the LUMO?"""
@@ -67,8 +68,7 @@ class GenericTDTest(unittest.TestCase):
                         t[0][2][0] == self.data.homos[0] + 1, t[0])
 
     @skipForParser('CFOUR','The parser is still being developed so we skip this test')
-    @skipForParser('Molcas','The parser is still being developed so we skip this test')
-    @skipForParser('Turbomole','The parser is still being developed so we skip this test')
+    @skipForParser('Molcas','The parser is still being developed so we skip this test')    
     def testsymsnumber(self):
         """Is the length of etsyms correct?"""
         self.assertEqual(len(self.data.etsyms), self.number)
@@ -82,6 +82,8 @@ class GenericTDTest(unittest.TestCase):
     @skipForParser('Jaguar', 'etrotats are not yet implemented')
     @skipForParser('NWChem', 'etrotats are not yet implemented')
     @skipForParser('QChem', 'Q-Chem cannot calculate rotatory strengths')
+    @skipForLogfile("Turbomole/basicTurbomole7.4/CO_cc2_TD", "Rotatory strengths are not currently available for ricc2")
+    @skipForLogfile("Turbomole/basicTurbomole7.4/CO_adc2_TD", "Rotatory strengths are not currently available for ricc2")
     def testrotatsnumber(self):
         """Is the length of etrotats correct?"""
         self.assertEqual(len(self.data.etrotats), self.number)
@@ -280,4 +282,4 @@ if __name__ =="__main__":
     from test_data import DataSuite
     suite = DataSuite(['TD'])
     suite.testall()
-
+    
